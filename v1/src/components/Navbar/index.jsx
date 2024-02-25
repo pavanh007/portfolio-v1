9fac0d98b4aeb7bb1c7997a6d7d27b890d9e5783
pavanh <pavanhdsce@gmail.com>
2024-02-25 11:37:18 +0530

    updated: navbar

--- v1/src/components/Navbar/index.jsx
+++ v1/src/components/Navbar/index.jsx
@@ -13,10 +13,8 @@ const Navbar = () => {
       }
     };
 
-    // Add event listener for scroll
     window.addEventListener("scroll", handleScroll);
 
-    // Cleanup function to remove event listener
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -25,56 +23,82 @@ const Navbar = () => {
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
-        alignContent: "center",
-        alignSelf: "center",
+        flexDirection: "row",
+        position: "fixed",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        background: "white",
+        zIndex: 10,
+        borderBottom: " 0.5px solid rgba(10, 20, 50, 0.1)",
+        background: "rgba(240, 240, 240, 0.5)",
+        backdropFilter: "blur(8px)",
       }}
     >
+      <div
+        style={{
+          display: "flex",
+          width: "33.33%",
+          justifyContent: "center",
+        }}
+      >
+        <button className="button_name" data-text="Awesome">
+          <span className="actual-text-name">&nbsp;PAVAN.H&nbsp;</span>
+          <span aria-hidden="true" className="hover-text-name">
+            &nbsp;PAVAN.H&nbsp;
+          </span>
+        </button>
+      </div>
       <div className={`navbar ${isScrolled ? "scrolled" : ""}`}>
         <div className="outerLayer">
           <div className="tabs">
-            <button className="tabs-tab-one">
-              <img
+            <button className="tabs-buttons">
+              {/* <img
                 src="https://i.ibb.co/0n6VN5Q/4092564-profile-about-mobile-ui-user-icon.png"
                 alt="4092564-profile-about-mobile-ui-user-icon"
                 border="0"
                 width={12}
                 height={12}
-              />
+              /> */}
               &nbsp;ABOUT
             </button>
-            <button className="tabs-tab-one">
-              <img
+            <button className="tabs-buttons">
+              {/* <img
                 src="https://i.ibb.co/d0Hn0PC/3669401-ic-work-icon.png"
                 alt="3669401-ic-work-icon"
                 border="0"
                 width={12}
                 height={12}
-              />
+              /> */}
               &nbsp;WORK
             </button>
-            <button className="tabs-tab-one">
-              <img
+            <button className="tabs-buttons">
+              {/* <img
                 src="https://i.ibb.co/tmBRWGk/4265047-bulb-creative-idea-lightbulb-startup-icon.png"
                 alt="4265047-bulb-creative-idea-lightbulb-startup-icon"
                 border="0"
                 width={12}
                 height={12}
-              />
+              /> */}
               &nbsp;PROJECT
             </button>
-            <button className="tabs-tab-one">
-              <img
+            <button className="tabs-buttons">
+              {/* <img
                 src="https://i.ibb.co/pb5MDvs/8665779-link-connection-icon.png"
                 alt="8665779-link-connection-icon"
                 border="0"
                 width={12}
                 height={12}
-              />
-              &nbsp;CONNECT
+              /> */}
+              &nbsp;Connect
             </button>
           </div>
         </div>
+      </div>
+      <div
+        style={{ display: "flex", width: "33.33%", justifyContent: "center" }}
+      >
+        <button className="button_touch">get in touch</button>
       </div>
     </div>
   );
